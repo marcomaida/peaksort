e578449d87d36f71f8b77e1ef3be17d2a20b9d7c
unknown <00marcomaida00@gmail.com>
2019-02-27 12:34:27 +0100

    stuff

--- AnchorSort/AnchorSort.h
+++ AnchorSort/AnchorSort.h
@@ -3,12 +3,12 @@ int main();
 struct cell;
 int * getRandom(int n, int max);
 void peakSort(int * nums, int n);
-void printArray(cell * cells, int n);
+void printArray(int * nums, cell * cells, int n);
 void printArray(int * nums, int n);
 int * sort(int * nums, int n);
-cell cmp(int l, int ldist, int x, int r, int rdist);
-bool isPeak(cell c);
-bool isZero(cell c);
+cell cmp(int * nums, int l, int x, int r);
+bool isPeak(int * nums, int n, cell * tableRow, int index);
+bool isLastCellOfRow(cell c, int index);
 int lCell(cell * row, int hSize, int index);
 int rCell(cell * row, int hSize, int index);
 
